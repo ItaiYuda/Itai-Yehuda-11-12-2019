49d89efaeb5e312d864574e47cb7f19ccbed0722
ItaiYuda <itaiyuda@gmail.com>
2019-12-17 02:24:10 +0200

    Updates

---
+++ precache-manifest.87099a1c10dce51c7d6105255bdd9596.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5dac57af60e7dbb2a1222224dc1dc09",
+    "url": "/Itai-Yehuda-11-12-2019/index.html"
+  },
+  {
+    "revision": "32c467ccace7e2f98bd2",
+    "url": "/Itai-Yehuda-11-12-2019/static/css/2.2944374d.chunk.css"
+  },
+  {
+    "revision": "d1ba515b4cb8f0ebc810",
+    "url": "/Itai-Yehuda-11-12-2019/static/css/main.d0228a5c.chunk.css"
+  },
+  {
+    "revision": "32c467ccace7e2f98bd2",
+    "url": "/Itai-Yehuda-11-12-2019/static/js/2.ff513bee.chunk.js"
+  },
+  {
+    "revision": "c4fc3062f1cb17c557a1b5daa66fdabb",
+    "url": "/Itai-Yehuda-11-12-2019/static/js/2.ff513bee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d1ba515b4cb8f0ebc810",
+    "url": "/Itai-Yehuda-11-12-2019/static/js/main.73f7328b.chunk.js"
+  },
+  {
+    "revision": "924683ef7dbcb7383780",
+    "url": "/Itai-Yehuda-11-12-2019/static/js/runtime-main.217a7d64.js"
+  },
+  {
+    "revision": "7961841815ecc957fd9234437ba4ab02",
+    "url": "/Itai-Yehuda-11-12-2019/static/media/infav.79618418.png"
+  },
+  {
+    "revision": "d033581e11e46659ba2478f765a4c407",
+    "url": "/Itai-Yehuda-11-12-2019/static/media/light.d033581e.png"
+  },
+  {
+    "revision": "f689d38adfd93baa60590a4a832abeaa",
+    "url": "/Itai-Yehuda-11-12-2019/static/media/notinfav.f689d38a.png"
+  },
+  {
+    "revision": "751512adf544e173234e34558b1aad77",
+    "url": "/Itai-Yehuda-11-12-2019/static/media/pagenotfound.751512ad.png"
+  }
+]);
